5de52fa4441b8acbb675d527c538d18acc61643e
argtang <8416172+argtang@users.noreply.github.com>
2022-01-12 21:34:07 +0100

    bump argo

--- deployment/argocd/install.ps1
+++ deployment/argocd/install.ps1
@@ -1,5 +1,5 @@
 Push-Location $PSScriptRoot
-helm upgrade --install argocd . --namespace argocd
+helm upgrade --install argocd . -f values.yaml --namespace argocd --debug > temp.t.yaml
 
 #pass k8wK3nPzwuRc4eFN
 # admin $2a$10$AqYS.3MT3w2tl/0g47IvK.Xdh8Idftwz//zIUf7oOS9nu.mHKZyMy
